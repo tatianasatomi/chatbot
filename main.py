3b9dd99eccd1cef02fd1f410de91936e8a354574
tatianasatomi <tatianasatomi@gmail.com>
2021-09-27 20:45:47 +0900

    Adicionei pergunta de tudo bem ao usuário

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ def cumprimento(nome):
 
 nome = input("Qual o seu nome? ")
 cumprimento(nome)
+
+como = input(f"Tudo bem, {nome}? ").lower()
+respostas = {"bem": "Que bom!", "mais ou menos": "Quer conversar sobre isso?", "mal": "Pouxa, o que aconteceu?", "bem mal": "Nossa, o que aconteceu? Quer conversar sobre isso?"}
+
+for resp in respostas:
+    if resp in como:
+        if como == "bem mal" and como != "bem" and como != "mal":
+            print(respostas["bem mal"])
+            break
+            
+        else:
+            print(respostas[resp])
+            break
