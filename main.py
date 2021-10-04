ec9b6c5fe57052ef3d66c190d4b4c89e67df56b3
tatianasatomi <tatianasatomi@gmail.com>
2021-10-04 20:29:15 +0900

    Bugfix filtro da resposta "bem mal"

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-def cumprimento(nome):
+def cumprimento(nome): #parâmetro
     print(f"Bom dia, {nome}!")
 
 nome = input("Qual o seu nome? ")
-cumprimento(nome)
+cumprimento(nome) #argumento
 
 como = input(f"Tudo bem, {nome}? ").lower()
 respostas = {"bem": "Que bom!", "mais ou menos": "Quer conversar sobre isso?", "mal": "Pouxa, o que aconteceu?", "bem mal": "Nossa, o que aconteceu? Quer conversar sobre isso?"}
 
-for resp in respostas:
-    if resp in como:
-        if como == "bem mal" and como != "bem" and como != "mal":
-            print(respostas["bem mal"])
-            break
-            
-        else:
-            print(respostas[resp])
-            break
+
+
+if "bem mal" in como:
+    print(respostas["bem mal"])
+else:
+    for resp in respostas: #itera dentro do valor, por isso ignora filtros anteriores
+        if "bem mal" not in resp:
+
+            if resp in como:
+                print(respostas[resp])    
+
