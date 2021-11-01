5576c64679c5ae450c7275f5e4b796863cb63ca3
tatianasatomi <tatianasatomi@gmail.com>
2021-11-01 21:03:46 +0900

    estudo de classes - orientação a objetos

---
+++ classe.py
@@ -0,0 +1,14 @@
+class Teste:
+    def __init__(self, par):
+        #pass
+        self.var = "Oi"
+        self.var1 = par
+
+    def funcao_classe(self):
+        print("Como vai?")
+    
+    def cafe(self, nome):
+        print(f"Vai um cafézinho aí, {nome}?")
+    
+    def forms(self, nome, idade):
+        print(f"Dados do usuário\n Nome: {nome}\n Idade: {idade}")
--- sessao.py
+++ sessao.py
@@ -1,4 +1,5 @@
 import modulo
+from classe import Teste
 
 print(modulo.x, modulo.y, modulo.z)
 
@@ -11,3 +12,15 @@ modulo.teste1("Final", "Fantasy")
 modulo.teste1(29.9, 16.45)
 
 modulo.teste2(1, 2, 3, 4)
+
+
+classe_teste = Teste("Tchau")
+print(classe_teste.var, classe_teste.var1)
+
+classe_teste.funcao_classe()
+
+nome = input("Qual o seu nome? ")
+classe_teste.cafe(nome)
+
+x, y = "Ale", 26
+classe_teste.forms(y, x)
