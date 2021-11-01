6a0b1090fed5373d746550cf833e6268916fef9c
tatianasatomi <tatianasatomi@gmail.com>
2021-11-01 20:36:57 +0900

    importação de módulos

---
+++ modulo.py
@@ -0,0 +1,13 @@
+x, y, z = 0, 1, 2
+
+def funcao():
+    print("Segunda-feira, 1º de novembro de 2021.")
+
+def teste(var):
+    print(var)
+
+def teste1(p, q):
+    print(p + q)
+
+def teste2(a, b, c, d):
+    print(f"Os valores são: {a}, {b}, {c} e {d}.")
---
+++ sessao.py
@@ -0,0 +1,13 @@
+import modulo
+
+print(modulo.x, modulo.y, modulo.z)
+
+modulo.funcao()
+
+modulo.teste("15º C")
+
+modulo.teste1("Final", "Fantasy")
+
+modulo.teste1(29.9, 16.45)
+
+modulo.teste2(1, 2, 3, 4)
